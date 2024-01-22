7eebf73a498609ccdeeedb8dda33838ed459a8f2
Moises Alonso <maalonso@uvg.edu.gt>
2024-01-22 07:44:54 -0600

    Correccion de ubicaciones

--- EjemploHT1/src/Calculadora1.java
+++ EjemploHT1/src/Calculadora1.java
@@ -1,10 +1,23 @@
 public class Calculadora1 implements ICalculator{
 
+    
+    /** 
+     * This class allow you to execute an addition of two integers
+     * @param n1 first number
+     * @param n2 second number
+     * @return int total
+     */
     @Override
     public int add(int n1, int n2) {
         return n1 + n2;
     }
 
+    /** 
+     * This class allow you to execute an sustraction of two integers
+     * @param n1 first number
+     * @param n2 second number
+     * @return int the difference
+     */
     @Override
     public int mult(int n1, int n2) {
         return n1 * n2;
--- EjemploHT1/src/ProgramUI.java
+++ EjemploHT1/src/ProgramUI.java
@@ -4,48 +4,38 @@ public class ProgramUI {
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ICalculator calculator = new Calculadora2();
-        int opcion;
 
-        do {
-            System.out.println("Menú:");
-            System.out.println("1. Realizar Suma");
-            System.out.println("2. Realizar Multiplicación");
-            System.out.println("3. Salir");
-            System.out.print("Seleccione una opción: ");
+        // Paso 2
+        System.out.print("Ingrese un número: ");
+        int numero = scanner.nextInt();
 
-            opcion = scanner.nextInt();
+        // Paso 3
+        int contador = 2;
 
-            switch (opcion) {
-                case 1:
-                    realizarOperacion(calculator, "suma");
-                    break;
-                case 2:
-                    realizarOperacion(calculator, "multiplicación");
-                    break;
-                case 3:
-                    System.out.println("Saliendo del programa. ¡Hasta luego!");
-                    break;
-                default:
-                    System.out.println("Opción no válida. Por favor, seleccione una opción válida.");
+        // Paso 4
+        if (numero > 1) {
+            // Paso 7
+            while (contador < numero) {
+                // Paso 4
+                if (numero % contador == 0) {
+                    // Paso 9
+                    System.out.println("No es un número primo");
+                    return;
+                }
+
+                // Paso 6
+                contador++;
             }
 
-        } while (opcion != 3);
+            // Paso 8
+            System.out.println("Es un número primo");
+        } else {
+            // Manejar el caso cuando el número es 1 o menor
+            System.out.println("No es un número primo");
+        }
 
+        // Paso 10
         scanner.close();
     }
-
-    private static void realizarOperacion(ICalculator calculator, String operacion) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese el primer número: ");
-        int num1 = scanner.nextInt();
-        System.out.print("Ingrese el segundo número: ");
-        int num2 = scanner.nextInt();
-
-        if (operacion.equals("suma")) {
-            System.out.println("Resultado de la suma: " + calculator.add(num1, num2));
-        } else if (operacion.equals("multiplicación")) {
-            System.out.println("Resultado de la multiplicación: " + calculator.mult(num1, num2));
-        }
-    }
+    
 }
