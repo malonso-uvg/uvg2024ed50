2f834a218e3d94592938601f106d89ec0a1edc15
Moises Alonso <maalonso@uvg.edu.gt>
2024-02-21 11:09:34 -0600

    Ejemplo Factory con usuarios

---
+++ EjemploFactory2/factory-design-pattern_example/src/edu/uvg/factory/NumeroRepetidoException.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package edu.uvg.factory;
+
+/**
+ * @author MAAG
+ *
+ */
+public class NumeroRepetidoException extends Exception {
+	
+	public NumeroRepetidoException(String message) {
+		super(message);
+	}
+}
