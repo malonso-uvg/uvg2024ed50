a265c7e71fea0c55aa297e27bf8d38bde3b23aa4
Moises Alonso <maalonso@uvg.edu.gt>
2024-05-06 07:41:33 -0600

    Ejemplo Neo4J en pagina web

---
+++ 13_WebApp/src/main/java/ConnectionData.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+
+/**
+ * @author MAAG
+ *
+ */
+public class ConnectionData {
+
+	public static final String username = "neo4j";
+	public static final String password = "densities-feelings-tubes";
+	public static final String boltURL = "bolt://44.192.99.82:7687";
+}
